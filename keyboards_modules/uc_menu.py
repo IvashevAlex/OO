5ca7968d33b03ea08f53d0170cc5e42f6c90b21e
Крылов Антон Юрьевич <krylov.au@kontur>
2021-02-20 21:14:23 +0500

    fix UC menu

--- keyboards_modules/uc_menu.py
+++ keyboards_modules/uc_menu.py
@@ -5,12 +5,12 @@ from WhiteList import bot
 
 
 def UC_menu(name, bot):
-    @bot.message_handler(func=lambda message: message.text == name)
-    def UC(message):
-      if modul_for_bot.practicks_data.get(message.from_user.id) != 'PR':
-        modul_for_bot.tests_data[message.chat.id] = 'UC'
-        modul_for_bot.sql_user(bot, message)
-        keyboards.test_menu(bot, message)
+  name = name + '1'
+  @bot.message_handler(func=lambda message: message.text == name)
+  def UC(message):
+    modul_for_bot.tests_data[message.chat.id] = 'UC'
+    modul_for_bot.sql_user(bot, message)
+    keyboards.test_menu(bot, message)
 
 
 def test_uc(bot, callback_query):  # <--- формируем меню с тестами для УЦ
