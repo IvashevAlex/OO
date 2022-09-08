7d9784c90aeb5ee92f2670f922cf0dc8f6b074ee
Ивашев Александр Сергеевич <ivashev@kontur>
2022-09-08 13:12:57 +0500

    add info about sending logic

--- send_info.py
+++ send_info.py
@@ -1,13 +1,9 @@
 import time
 from WhiteList import bot
 
-if time.gmtime()[4] % 3 == 0:
-    if time.gmtime()[5] < 5:
-        print('Пришло время для 3-х минутной рассылки!')
-        bot.send_message(5484457194, 'Пришло время для 3-х минутной рассылки!')
-        time.sleep(5)
-if time.gmtime()[4] % 5 == 0:
-    if time.gmtime()[5] < 5:
-        print('Пришло время для 5-ти минутной рассылки!')
-        bot.send_message(5737229331, 'Пришло время для 5-ти минутной рассылки!')
-        time.sleep(5)
+# if time.gmtime()[4] % 3 == 0:
+#     if time.gmtime()[5] < 5:
+#         print('Пришло время для 3-х минутной рассылки!')
+#         bot.send_message(5484457194, 'Пришло время для 3-х минутной рассылки!')
+#         time.sleep(5)
+
