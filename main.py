693534eaa8ad5048a541178d54b91d5194f0d27f
Ивашев Александр Сергеевич <ivashev@kontur>
2022-09-13 13:37:14 +0500

    add prints in main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ test_mode = test_mode_check.test_mode()
 # Обработка команды /start
 @bot.message_handler(commands=["start"])
 def greeting(message):
+    print('IN greeting')
     if echo(message) == True:
         question(bot, message)
 
@@ -18,6 +19,7 @@ def greeting(message):
 # Обработка команды /help
 @bot.message_handler(commands=["help"])
 def help(message):
+    print('IN help')
     if echo(message) == True:
         bot.send_message(message.chat.id, info, parse_mode='Markdown')
 
@@ -25,6 +27,7 @@ def help(message):
 # Обработка команды /admin
 @bot.message_handler(commands=["admin"])
 def admin_menu(message):
+    print('IN admin_menu')
     if message.chat.id in (233770916, 391368365, 1325029854):
         Admin_menu(message, bot)
     else:
@@ -32,20 +35,23 @@ def admin_menu(message):
 
 # Обработка команды /test_1
 @bot.message_handler(commands=["test_1"])
-def help(message):
+def test(message):
+    print('IN test')
     if echo(message) == True:
         bot.send_message(message.chat.id, text.test_1, parse_mode='Markdown')
 
 # Обработка текстового сообщения "В меню"
 @bot.message_handler(func=lambda message: message.text == "В меню")
 def back(message):
+    print('IN back')
     if echo(message) == True:
         question(bot, message)
 
 
 # Обработка текстового сообщения "Назад"
 @bot.message_handler(func=lambda message: message.text == "Назад")
-def back(message):
+def back_menu(message):
+    print('IN back_menu')
     if echo(message) == True:
         # Запускаем test_menu из modul_for_bot
         back_to_menu(bot, message)
@@ -54,6 +60,7 @@ def back(message):
 # Обработка текстового сообщения "Помощь"
 @bot.message_handler(func=lambda message: message.text == "Помощь")
 def help_text(message):
+    print('IN help_text')
     if echo(message) == True:
         bot.send_message(message.chat.id, info, parse_mode='Markdown')
 
@@ -61,6 +68,7 @@ def help_text(message):
 # Обработка текстового сообщения "Результаты"
 @bot.message_handler(func=lambda message: message.text == "Результаты")
 def res0(message):
+    print('IN res0')
     if echo(message) == True:
         res(bot, message)
 
@@ -68,6 +76,7 @@ def res0(message):
 # Обработка нажатия кнопки "Сообщить об ошибке"
 @bot.callback_query_handler(func=lambda callback_query: callback_query.data == 'Сообщить об ошибке')
 def error_send(callback_query):
+    print('IN error_send')
     if echo(callback_query) == True:
         send_error(bot, callback_query)
 
@@ -81,7 +90,7 @@ def error_send(callback_query):
 # В среднем, считаем, что это ответы на вопросы, заданные пользователю.
 @bot.message_handler(content_types=['text'])
 def answer0(message):
-
+    print('IN answer0')
     if callback_check.get(message.from_user.id) == 'tests':
         continue_(bot, message)
         messages = bot.send_message(message.from_user.id, text.waiting_check)
@@ -110,7 +119,7 @@ def answer0(message):
 
 
 # Информация по боту 
-print(f'Бот запущен! Текущая версия {ver}')
+print(f'Бот запущен! Текущая версия {ver}.')
 if test_mode == True:
     print('Активирован режим тестирования!')
 else:
