2623a426d64a2e50588a164084a9bdebee57b585
Ivashev Aleksandr <ivashev@skbkontur.ru>
2024-01-16 10:10:56 +0500

    add token check and token request

--- get_staff_api.py
+++ get_staff_api.py
@@ -6,7 +6,7 @@ def staff_api_users(token):
     value_auth = 'Bearer ' + token
     url = config.staff_api_users_url
     method = config.staff_api_users_method
-    payload = config.staff_api_users_payload
+    payload = {}
     headers = {'Authorization': value_auth, 'Cookie': config.staff_api_users_Cookie}
     params = config.staff_api_users_params
 
@@ -18,14 +18,58 @@ def staff_api_users(token):
         file = open('data.txt', 'w+', encoding='utf-8')
         file.write(data)
         file.close()
-        print('Запрос к АПИ выполнен успешно.')
+        print('Запрос к АПИ выполнен успешно')
 
     except Exception as EX:
-        print('Ошибка запроса данных по АПИ!')
+        print('Ошибка запроса данных по АПИ')
         print(EX.args)
+
+
+def checking_request():
+
+    url = config.checking_request_url
+    payload = config.checking_request_payload
+    headers = {'Authorization': config.checking_request_authorization,
+                'Content-Type': config.checking_request_content_type,
+                'Cookie': config.checking_request_cookie}
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        data = json.loads(response.text).get("access_token")
+        file = open('token.txt','w')
+        file.write(data)
+        file.close()
+        print('Новый токен успешно получен')
+    
+    except Exception as EX:
+        print('Ошибка запроса нового токена')
+        print(EX.args)
+
+
+def check_token(token):
+
+    value_auth = 'Bearer ' + token
+    url = config.check_token_url
+    payload={}
+    headers = {'Authorization': value_auth,'Cookie': config.check_token_headers_cookie}
+
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        if response.status_code == 401:
+            print('Запрос нового токена')
+            checking_request()
+        else:
+            print('Код ответа:', response.status_code)
+
+    except Exception as EX:
+        print('Ошибка проверки доступа по токену')
+        print(EX.args)
+
 
 file = open('token.txt','r')
 token = file.read()
 file.close()
 
+check_token(token)
 staff_api_users(token)
